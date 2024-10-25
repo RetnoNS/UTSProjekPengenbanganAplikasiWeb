0a3b4043c929952ab0dcdf0ebf8d627d34651b16
RetnoNS <119503025+RetnoNS@users.noreply.github.com>
2024-10-25 14:24:52 +0700

    Initial commit

---
+++ app/src/main/java/lat/pam/utsproject/OrderActivity.kt
@@ -0,0 +1,31 @@
+package lat.pam.utsproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_order)
+
+        val buttonPlaceOrder = findViewById<Button>(R.id.btnOrder)
+
+        buttonPlaceOrder.setOnClickListener(View.OnClickListener{
+            val intent = Intent(this, ConfirmationActivity::class.java)
+            startActivity(intent)
+        })
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
